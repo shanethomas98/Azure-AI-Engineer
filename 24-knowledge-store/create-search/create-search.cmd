5fe779494e60be227edd36da20183eacb8f98100
shanethomas98 <shanethomas199@gmail.com>
2021-05-14 07:45:21 -0700

    Last Practical and files for last 3 folders

---
+++ 24-knowledge-store/create-search/create-search.cmd
@@ -0,0 +1,24 @@
+@echo off
+
+rem Set values for your Search service
+set url=https://ai102srch2412927444.search.windows.net
+set admin_key=9CA2D2BC57939633254BDE51D352336B
+
+echo -----
+echo Creating the data source...
+call curl -X POST %url%/datasources?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @data_source.json
+
+echo -----
+echo Creating the skillset...
+call curl -X PUT %url%/skillsets/margies-knowledge-skillset?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @skillset.json
+
+echo -----
+echo Creating the index...
+call curl -X PUT %url%/indexes/margies-knowledge-index?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @index.json
+
+rem wait
+timeout /t 3 /nobreak
+
+echo -----
+echo Creating the indexer...
+call curl -X PUT %url%/indexers/margies-knowledge-indexer?api-version=2020-06-30 -H "Content-Type: application/json" -H "api-key: %admin_key%" -d @indexer.json
